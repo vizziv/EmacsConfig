3119472ad570b31cd82c21f2aa2f40dff6177b3e
Ziv Scully <zivscully@gmail.com>
2023-10-26 23:09:41 -0400

    Remove Ur/Web and Neuron.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,197 @@
 ;; -*- lexical-binding: t; -*-
 
+;; Package management: straight.el and use-package
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(setq straight-use-package-by-default t)
+(straight-use-package 'use-package)
+
+;; Enable upcase and downcase.
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+
+;; Doom themes and modeline
+(use-package doom-themes
+  :config
+  (load-theme 'doom-gruvbox t))
+(use-package doom-modeline
+  :hook
+  (window-setup . doom-modeline-mode))
+(use-package all-the-icons
+  :custom
+  (doom-modeline-buffer-file-name-style 'truncate-upto-root)
+  (doom-modeline-height 27))
+
+;; Autosaves
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Path and shell
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns x))
+  :config
+  (exec-path-from-shell-initialize))
+(setq explicit-bash-args (quote ("--noediting" "--login" "-i")))
+
+;; Dired
+(setq insert-directory-program (executable-find "gls"))
+
+;; Remove trailing whitespace
+(defun delete-trailing-whitespace-except-current-line ()
+  "do delete-trailing-whitespace, except preserve whitespace of current line"
+  (interactive)
+  (let ((current-line (buffer-substring (line-beginning-position) (line-end-position)))
+        (backward (- (line-end-position) (point))))
+    (delete-trailing-whitespace)
+    (when (not (string-equal (buffer-substring (line-beginning-position) (line-end-position))
+                             current-line))
+      (delete-region (line-beginning-position) (line-end-position))
+      (insert current-line)
+      (backward-char backward))))
+(add-hook 'before-save-hook 'delete-trailing-whitespace-except-current-line)
+
+;; Smart indenting in visual-line-mode.
+(use-package adaptive-wrap)
+
+;; Git
+(use-package magit
+  :bind ("C-x g" . magit-status))
+
+;; Multiple cursors
+(use-package multiple-cursors
+  :bind (("C-;" . mc/mark-next-like-this)
+         ("C-'" . mc/mark-previous-like-this)
+         ("C-M-;" . mc/mark-all-like-this)))
+(use-package multiple-cursors-core
+  :straight multiple-cursors
+  :bind (:map mc/keymap
+         ("<return>" . nil)))
+
+;; Ivy, Counsel, and Swiper
+(defun ivy--quote-plus (string)
+  "Build a regexp suitable for Ivy from STRING.
+Convert whitespace to \".*\" and `regexp-quote' everything else."
+  (mapconcat #'regexp-quote (split-string string) ".*"))
+(defun ivy--quote-single-space (string)
+  "Build a regexp suitable for Ivy from STRING.
+Has a similar effect to `ivy--quote-plus', except a single space
+is quoted, to allow searching for multi-word phrases."
+  (mapconcat #'regexp-quote (split-string string "[\f\t\n\r\v]+\\|  +" t) ".*"))
+(defun ivy--quote-ignore-order (string)
+  "Build a regexp alist suitable for Ivy from STRING.
+Has a similar effect to `ivy--quote-with-space', except the order of
+whitespace-separated substrings is ignored."
+  (mapcar (lambda (substring)
+            (cons (regexp-quote substring) t))
+          (split-string string)))
+(use-package ivy
+  :config
+  (ivy-mode)
+  (setcdr (assoc t ivy-re-builders-alist) #'ivy--quote-single-space)
+  (add-to-list 'ivy-preferred-re-builders (cons #'ivy--quote-plus "quote"))
+  (add-to-list 'ivy-preferred-re-builders (cons #'ivy--quote-single-space "quote space"))
+  (add-to-list 'ivy-preferred-re-builders (cons #'ivy--quote-ignore-order "quote order")))
+(use-package counsel
+  :bind (("C-x C-r" . counsel-recentf))
+  :config
+  (counsel-mode)
+  (setcdr (assoc 'counsel-M-x ivy-initial-inputs-alist) ""))
+(use-package swiper
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper)))
+
+;; Company
+(use-package company)
+
+;; Flycheck
+(use-package flycheck)
+
+;; ;; LSP
+(use-package eglot
+  :hook ((haskell-mode rustic-mode) . eglot-ensure))
+;; (use-package lsp-mode
+;;   :straight (lsp-mode :type git :host github :repo "emacs-lsp/lsp-mode" :files (:defaults "clients/*.el"))
+;;   :config
+;;   (add-hook 'lsp-mode-hook 'flycheck-mode)
+;;   (add-hook 'lsp-mode-hook 'company-mode))
+
+;; Spell checking
+(defun flycheck-maybe-recheck (_)
+  (when (bound-and-true-p flycheck-mode)
+   (flycheck-buffer)))
+(use-package flycheck-aspell
+  :straight (flycheck-aspell :type git :host github :repo "leotaku/flycheck-aspell")
+  :config
+  (flycheck-aspell-define-checker "text"
+    "Text" ("--add-filter" "url")
+    (text-mode fundamental-mode))
+  (add-to-list 'flycheck-checkers 'tex-aspell-dynamic)
+  (add-to-list 'flycheck-checkers 'markdown-aspell-dynamic)
+  (add-to-list 'flycheck-checkers 'text-aspell-dynamic)
+  (advice-add 'ispell-pdict-save :after 'flycheck-maybe-recheck))
+
+;; LaTeX
+(use-package tex
+  :straight auctex
+  :bind (:map TeX-mode-map
+         ("\"" . nil))
+  :mode (".tex" . LaTeX-mode)
+  :config
+  (add-hook 'LaTeX-mode-hook 'reftex-mode)
+  (add-hook 'LaTeX-mode-hook 'flycheck-mode)
+  (add-hook 'LaTeX-mode-hook 'adaptive-wrap-prefix-mode))
+
+;; BibTeX
+;; For some reason, straight couldn't find bibtex-completion on MELPA, so using manual recipe.
+(use-package bibtex-completion
+  :straight (bibtex-completion :type git :host github :repo "tmalsburg/helm-bibtex"))
+(use-package ivy-bibtex
+  :straight (ivy-bibtex :type git :host github :repo "tmalsburg/helm-bibtex")
+  :after reftex
+  :bind (:map reftex-mode-map
+         ("C-c [" . ivy-bibtex-with-local-bibliography))
+  :config
+  (add-to-list 'ivy-re-builders-alist '(ivy-bibtex . ivy--regex-ignore-order)))
+
+;; Haskell
+(use-package haskell-mode)
+
+;; Rust
+(use-package cargo
+  :hook (rustic-mode . cargo-minor-mode))
+(use-package rustic)
+
+;; Markdown
+(use-package markdown-mode
+  :config
+  (add-hook 'markdown-mode 'flycheck-mode))
+
+;; Zoom all frames at once
+(use-package zoom-frm
+  :bind (("C-x C-+" . zoom-frm-in)
+         ("C-x C-=" . zoom-frm-in)
+         ("C-x C--" . zoom-frm-out)
+         ("C-x C-0" . zoom-frm-unzoom)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(TeX-parse-self t)
+ '(adaptive-fill-regexp "[ 	]*\\(\\\\*[-–!|#%;>*·•‣⁃◦]+[ 	]*\\)*")
  '(bibtex-completion-additional-search-fields '("journal" "booktitle"))
  '(bibtex-completion-bibliography "refs.bib")
  '(bibtex-completion-cite-default-command "citep")
@@ -19,6 +205,7 @@
  '(bibtex-completion-pdf-field "file")
  '(column-number-mode t)
  '(enable-remote-dir-locals t)
+ '(exec-path-from-shell-arguments '("-l"))
  '(flycheck-checker-error-threshold 1024)
  '(global-visual-line-mode t)
  '(indent-tabs-mode nil)
@@ -31,6 +218,7 @@
  '(neuron-default-zettelkasten-directory "~/Documents/Zettelkasten")
  '(ns-alternate-modifier 'super)
  '(ns-command-modifier 'meta)
+ '(ns-pop-up-frames nil)
  '(recentf-max-menu-items 200)
  '(recentf-max-saved-items 200)
  '(recentf-mode t)
@@ -81,200 +269,3 @@
  '(font-latex-sectioning-5-face ((t (:foreground "#fc689d" :weight semi-bold))))
  '(ivy-modified-buffer ((t (:inherit bold :foreground "#ada296"))))
  '(swiper-match-face-1 ((t (:background "#1B2229" :foreground "#ada296")))))
-
-;; Enable upcase and downcase.
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-
-;; Package management: straight.el and use-package
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-(setq straight-use-package-by-default t)
-(straight-use-package 'use-package)
-
-;; Aesthetics
-(use-package doom-themes
-  :config
-  (load-theme 'doom-gruvbox t))
-(use-package doom-modeline
-  :config
-  (doom-modeline-mode 1))
-(use-package all-the-icons
-  :custom
-  (doom-modeline-buffer-file-name-style 'truncate-upto-root)
-  (doom-modeline-height 27))
-
-;; Autosaves
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-
-;; Path and shell
-(use-package exec-path-from-shell
-  :if (memq window-system '(mac ns x))
-  :config
-  (exec-path-from-shell-initialize))
-(setq explicit-bash-args (quote ("--noediting" "--login" "-i")))
-
-;; Dired
-(setq insert-directory-program (executable-find "gls"))
-
-;; Remove trailing whitespace
-(defun delete-trailing-whitespace-except-current-line ()
-  "do delete-trailing-whitespace, except preserve whitespace of current line"
-  (interactive)
-  (let ((current-line (buffer-substring (line-beginning-position) (line-end-position)))
-        (backward (- (line-end-position) (point))))
-    (delete-trailing-whitespace)
-    (when (not (string-equal (buffer-substring (line-beginning-position) (line-end-position))
-                             current-line))
-      (delete-region (line-beginning-position) (line-end-position))
-      (insert current-line)
-      (backward-char backward))))
-(add-hook 'before-save-hook 'delete-trailing-whitespace-except-current-line)
-
-;; Smart indenting in visual-line-mode.
-(use-package adaptive-wrap)
-
-;; Git
-(use-package magit
-  :bind ("C-x g" . magit-status))
-
-;; Multiple cursors
-(use-package multiple-cursors
-  :bind (("C-;" . mc/mark-next-like-this)
-         ("C-'" . mc/mark-previous-like-this)
-         ("C-M-;" . mc/mark-all-like-this)))
-
-;; Ivy, Counsel, and Swiper
-(defun ivy--regexp-quote-plus (string)
-  "Build a regexp suitable for Ivy from STRING.
-Convert whitespace to \".*\" and `regexp-quote' everything else."
-  (mapconcat #'regexp-quote (split-string string) ".*"))
-
-(defun ivy--regexp-quote-ignore-order (string)
-  "Build a regexp alist suitable for Ivy from STRING.
-Has a similar effect to `my-regexp-quote', except the order of
-whitespace-separated substrings is ignored."
-  (mapcar (lambda (substring)
-            (cons (regexp-quote substring) t))
-          (split-string string)))
-
-(use-package ivy
-  :config
-  (ivy-mode)
-  (setcdr (assoc t ivy-re-builders-alist) #'ivy--regexp-quote-plus)
-  (add-to-list 'ivy-preferred-re-builders (cons #'ivy--regexp-quote-plus "quote"))
-  (add-to-list 'ivy-preferred-re-builders (cons #'ivy--regexp-quote-ignore-order "quote order")))
-(use-package counsel
-  :bind (("C-x C-r" . counsel-recentf))
-  :config
-  (counsel-mode)
-  (setcdr (assoc 'counsel-M-x ivy-initial-inputs-alist) ""))
-(use-package swiper
-  :bind (("C-s" . swiper)
-         ("C-r" . swiper)))
-
-;; Company, Flycheck, and LSP
-(use-package company)
-(use-package flycheck)
-(use-package lsp-mode
-  :straight (lsp-mode :type git :host github :repo "emacs-lsp/lsp-mode" :files (:defaults "clients/*.el"))
-  :config
-  (add-hook 'lsp-mode-hook 'flycheck-mode)
-  (add-hook 'lsp-mode-hook 'company-mode))
-
-;; Spell checking
-(defun flycheck-maybe-recheck (_)
-  (when (bound-and-true-p flycheck-mode)
-   (flycheck-buffer)))
-(use-package flycheck-aspell
-  :straight (flycheck-aspell :type git :host github :repo "leotaku/flycheck-aspell")
-  :config
-  (flycheck-aspell-define-checker "text"
-    "Text" ("--add-filter" "url")
-    (text-mode fundamental-mode))
-  (add-to-list 'flycheck-checkers 'tex-aspell-dynamic)
-  (add-to-list 'flycheck-checkers 'markdown-aspell-dynamic)
-  (add-to-list 'flycheck-checkers 'text-aspell-dynamic)
-  (advice-add 'ispell-pdict-save :after 'flycheck-maybe-recheck))
-
-;; LaTeX
-(use-package tex
-  :straight auctex
-  :bind (:map TeX-mode-map
-              ("\"" . nil))
-  :config
-  (add-hook 'LaTeX-mode-hook 'reftex-mode)
-  (add-hook 'LaTeX-mode-hook 'flycheck-mode)
-  (add-hook 'LaTeX-mode-hook 'adaptive-wrap-prefix-mode))
-
-;; BibTeX
-;; For some reason, straight couldn't find bibtex-completion on MELPA, so using manual recipe.
-(use-package bibtex-completion
-  :straight (bibtex-completion :type git :host github :repo "tmalsburg/helm-bibtex"))
-(use-package ivy-bibtex
-  :straight (ivy-bibtex :type git :host github :repo "tmalsburg/helm-bibtex")
-  :after reftex
-  :bind (:map reftex-mode-map
-              ("C-c [" . ivy-bibtex-with-local-bibliography))
-  :config
-  (add-to-list 'ivy-re-builders-alist '(ivy-bibtex . ivy--regex-ignore-order)))
-
-;; Haskell
-(use-package haskell-mode)
-
-;; Rust
-(use-package cargo)
-(use-package rustic
-  :config
-  (add-hook 'rustic-mode-hook 'lsp-mode)
-  (add-hook 'rustic-mode-hook 'cargo-mode))
-
-;; Markdown
-(use-package markdown-mode
-  :config
-  (add-hook 'markdown-mode 'flycheck-mode))
-
-;; Proof General
-(use-package proof-general)
-
-;; Zoom all frames at once
-(use-package zoom-frm
-  :bind (("C-x C-+" . zoom-frm-in)
-         ("C-x C-=" . zoom-frm-in)
-         ("C-x C--" . zoom-frm-out)
-         ("C-x C-0" . zoom-frm-unzoom)))
-
-;; UrWeb
-(let ((urweb-mode-dir "~/.emacs.d/usr/urweb-mode"))
-  (if (file-readable-p urweb-mode-dir)
-      (progn
-	(add-to-list 'load-path urweb-mode-dir)
-	(load "urweb-mode-startup"))))
-
-;; Neuron
-(let ((neuron-mode-dir "~/.emacs.d/usr/neuron-mode"))
-  (if (file-readable-p neuron-mode-dir)
-      (progn
-	(add-to-list 'load-path neuron-mode-dir)
-	(load "neuron-mode")
-        (flycheck-aspell-define-checker "neuron"
-          "Neuron" ("--add-filter" "url" "--add-filter" "markdown")
-          (neuron-mode))
-        (add-to-list 'flycheck-checkers 'neuron-aspell-dynamic)
-        (define-key neuron-mode-map (kbd "C-c C-;") 'neuron-toggle-connection-type)
-        (add-hook 'neuron-mode-hook 'company-mode)
-        (add-hook 'neuron-mode-hook 'company-neuron-setup)
-        (add-hook 'neuron-mode-hook 'flycheck-mode))))
